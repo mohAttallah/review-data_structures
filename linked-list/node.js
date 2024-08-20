7b2b54db9700add8656860239c3d24b42a7506ff
mohAttallah <mohmohmoud28@gmail.com>
2024-08-20 08:56:13 +0000

    linked list

---
+++ linked-list/node.js
@@ -0,0 +1,8 @@
+class Node {
+    constructor(new_data) {
+        this.data = new_data;
+        this.next = null;
+    }
+}
+
+module.exports = Node;
---
+++ linked-list/singlyLinkedList.js
@@ -0,0 +1,26 @@
+const Node = require('./node.js');
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+}
+
+
+const list1 = new SinglyLinkedList();
